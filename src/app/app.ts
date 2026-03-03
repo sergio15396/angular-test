70c695ea36a13c0cc1656afc8f91228614194d16
Sergio <sergio.reyes@insbaixcamp.cat>
2026-03-03 18:01:35 +0100

    @if, @for

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FormsModule } from '@angular/forms';
-import { CommonModule } from '@angular/common';
 
 import { Header } from './layout/header/header';
 
 @Component({
   selector: 'app-root',
-  imports: [Header, RouterOutlet, FormsModule, CommonModule],
+  imports: [Header, RouterOutlet, FormsModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
